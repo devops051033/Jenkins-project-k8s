f9159977d2f80079cef8bb59be040224922176f5
Abdullah Almamun <abdullah@Abdullahs-MBP.lan>
2024-10-07 13:25:26 -0400

    acceptance-test.js updated to get more log

--- acceptance-test.js
+++ acceptance-test.js
@@ -1,13 +1,31 @@
-import http from "k6/http";
-import { sleep } from "k6";
+import http from 'k6/http';
+import { sleep } from 'k6';
+import { Rate, Trend } from 'k6/metrics';
+
+// Define custom metrics
+const successRate = new Rate('successful_requests_below_20ms');
+const requestDuration = new Trend('http_request_duration'); // Custom trend for tracking request durations
+
 export const options = {
-  vus: 10,
-  duration: "10s",
+  vus: 200,
+  duration: '10s',
   thresholds: {
-    http_req_duration: ["p(90)<10000"], // 90 percent of response times must be below 600ms
+    successful_requests_below_20ms: ['rate>0.90'], // 90% of requests should be below 20ms
+    http_request_duration: ['p(90)<20'],           // 90% of request durations should be below 20ms
   },
 };
+
 export default function () {
-  http.get(`http://${__ENV.SERVICE}`);
+  const res = http.get(`http://${__ENV.SERVICE}`);
+  
+  // Track whether the response time is below 20ms
+  successRate.add(res.timings.duration < 20);
+  
+  // Track the request duration
+  requestDuration.add(res.timings.duration);
+  
   sleep(1);
 }
+
+
+
